f9973588d44c21544b5ed0d854d87ec2ff52ca02
dannyrg95 <dannyrg-@hotmail.com>
2024-05-31 21:59:38 -0600

    Version Inicial

---
+++ JN_WEB_TEST/JN_WEB_TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JN_WEB_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ JN_WEB_TEST_API/JN_WEB_TEST_API/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_WEB_TEST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConsultarUsuarios")]
+        public IActionResult ConsultarUsuarios()
+        {
+            var x = true;
+
+            if (x)
+            {
+                return NotFound("No se encontro el registro");
+            }
+
+            return Ok("Todo bien");
+        }
+
+    }
+}
